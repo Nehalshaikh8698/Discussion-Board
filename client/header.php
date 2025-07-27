d6cd068d75c6ae75dc790cd84f318557999a9c25
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2025-07-27 16:46:41 +0530

    Initial commit: Upload Discussion Board project

---
+++ client/header.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="./public/images/logo.png" alt="" srcset="">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Login</a>
+        <a class="nav-link" href="#">Category</a>
+        <a class="nav-link" href="#">Latest Quetions</a>
+
+      </div>
+    </div>
+  </div>
+</nav>
---
+++ client/signUp.php
@@ -0,0 +1,26 @@
+<div class="container">
+<h1 class="heading">signUp</h1>
+
+<form>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" placeholder="Enter username">
+  </div>
+   <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="email" class="form-label">User email</label>
+    <input type="text" class="form-control" id="email" placeholder="Enter user email">
+  </div>
+   <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="password" class="form-label">User Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Enter user password">
+  </div>
+   <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="address" class="form-label">User address</label>
+    <input type="text" class="form-control" id="address" placeholder="Enter user address">
+  </div>
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</form>
+</div>
