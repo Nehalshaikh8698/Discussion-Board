79efd8695c976abb22cdb3507471335520d28c42
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2025-07-29 20:33:21 +0530

    update

---
+++ client/ask.php
@@ -0,0 +1,18 @@
+<div class="container">
+<h1 class="heading">Ask Quetions</h1>
+
+<form action="./server/requests.php" method="post">
+ 
+   <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="email" class="form-label">User email</label>
+    <input type="text" name="email" class="form-control" id="email" placeholder="Enter user email">
+  </div>
+   <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="password" class="form-label">User Password</label>
+    <input type="password" name="password" class="form-control" id="password" placeholder="Enter user password">
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+  <button type="submit" name="login" class="btn btn-primary">submit</button>
+  </div>
+</form>
+</div>
